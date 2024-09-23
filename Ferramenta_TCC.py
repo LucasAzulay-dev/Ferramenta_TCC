1202c8b95c1e9b0d7688b3202f04abeff7bc79d5
Lucas Azulay <lsa6@cin.ufpe.br>
2024-09-23 11:14:08 -0300

    Mockup_Lucas_v0.2

---
+++ Ferramenta_TCC.py
@@ -0,0 +1,31 @@
+import subprocess
+from Teste_Driver_Creator import Create_Test_Driver
+from instrument_code import Create_Intrumented_Code
+
+# Defina o caminho para o arquivo Excel
+excel_file_path = "testvec1.xlsx"
+
+# Defina o nome do arquivo .c do SUT
+code_path = "SUT.c"
+
+Create_Intrumented_Code(code_path)
+
+# Defina o caminho para o arquivo .c do SUT instrumentado
+instrumented_c_path = "SUT.c"     #Substituir por instrumented_SUT.c
+
+# Defina o caminho para o arquivo .h do SUT instrumentado
+instrumented_h_path = "SUT.h"     #Substituir por instrumented_SUT.h
+
+# Defina o nome da função testada
+function_name = "SUT"
+
+# Defina a quantidade de outputs
+num_output = 1
+
+Create_Test_Driver(excel_file_path, instrumented_h_path, function_name, num_output)  #FI5 não coberto
+
+#Compila o programa C
+subprocess.run(["gcc", instrumented_c_path, "Test_Driver.c", "-o", "Test_Driver"], check=True)
+
+# Executa o programa C
+subprocess.run([".\Test_Driver.exe"], check=True)
---
+++ SUT.c
@@ -0,0 +1,30 @@
+//# include "../CompA/CompA.h"
+//# include "../CompB/CompB.h"
+//# include "../CompC/CompC.h"
+//# include "../CompD/CompD.h"
+
+void CompA (int AI1, int AI2, int AI3, int *AO1, int *AO2){
+    *AO1 = AI1 + AI2;
+    *AO2 = AI1 * AI3;
+}
+
+void CompB (int BI1, int BI2, int BI3, int BI4, int *BO1){
+    *BO1 = (BI1+BI2)*(BI3+BI4);
+}
+
+void CompC (int CI1, int CI2, int *CO1){
+    *CO1 = (CI1+CI2)*CI1;
+}
+
+void CompD (int DI1, int DI2, int *DO1){
+    *DO1 = (DI1-DI2)*DI2;
+}
+
+int AO1, AO2, BO1, CO1;
+
+void SUT(int SUTI1, int SUTI2, int SUTI3, int SUTI4, int SUTI5, int SUTI6, int SUTI7, int *SUTO1){
+    CompA(SUTI1, SUTI2, SUTI3, &AO1, &AO2);
+    CompB(SUTI4, SUTI5, SUTI6, SUTI7, &BO1);
+    CompC(AO1, BO1, &CO1);
+    CompD(CO1, AO2, SUTO1);    
+}

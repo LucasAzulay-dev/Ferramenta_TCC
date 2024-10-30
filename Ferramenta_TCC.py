ab646a11b2ba7b78d69eb7c08f5515c747c5ed7a
Andressa Rocha <sabrinagym11@hotmail.com>
2024-10-30 14:50:17 -0400

    Inicial

--- Ferramenta_TCC.py
+++ Ferramenta_TCC.py
@@ -4,23 +4,24 @@ from run_test_drive import Run_Test_Driver
 
 from Parser import gerar_arquivo_h_com_pycparser  #Mudar para instrument_code.py
 
-# Defina o caminho para o arquivo Excel
-excel_file_path = "testvec2.xlsx"
+# # Defina o caminho para o arquivo Excel
+# excel_file_path = "testvec2.xlsx"
 
-# Defina o nome do arquivo .c do SUT
-code_path = "SUT.c"
+# # Defina o nome do arquivo .c do SUT
+# code_path = "SUT.c"
 
-# Defina o nome da função testada
-function_name = "SUT"
+# # Defina o nome da função testada
+# function_name = "SUT"
 
-#Tipo de compilador
-compiler = "gcc"    #gcc ou clang
+# #Tipo de compilador
+# compiler = "gcc"    #gcc ou clang
 
-Create_Instrumented_Code(code_path)
+def executar_ferramenta(excel_file_path, code_path, function_name, compiler):   
+    Create_Instrumented_Code(code_path)
 
-gerar_arquivo_h_com_pycparser(code_path) #Mudar para instrument_code.py
+    gerar_arquivo_h_com_pycparser(code_path) #Mudar para instrument_code.py
 
-Create_Test_Driver(excel_file_path, function_name, code_path)  #FI5 não coberto
+    Create_Test_Driver(excel_file_path, function_name, code_path)  #FI5 não coberto
 
-Run_Test_Driver(compiler)
+    Run_Test_Driver(compiler)
 

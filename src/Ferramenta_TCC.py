928faea759fc33dd11a72cba7eb5d4e816da1e17
Lucas Azulay <lsa6@cin.ufpe.br>
2024-11-11 16:51:02 -0300

    Mensagens de erros adicionadas

--- src/Ferramenta_TCC.py
+++ src/Ferramenta_TCC.py
@@ -2,17 +2,19 @@ from Teste_Driver_Creator import Create_Test_Driver
 from instrument_code import Create_Instrumented_Code
 from run_test_drive import Run_Test_Driver
 from dc_cc import DC_CC_Report_Generator
-#from Parser import gerar_arquivo_h_com_pycparser  #Está em instrument_code.py
 from utils import adicionar_ao_log
 
 def executar_ferramenta(excel_file_path, code_path, function_name, compiler):  
     Create_Instrumented_Code(code_path)
 
-    #gerar_arquivo_h_com_pycparser(code_path) #Está em instrument_code.py // 
+    error = Create_Test_Driver(excel_file_path, function_name, code_path)  #FI5 parcialmente coberto
 
-    Create_Test_Driver(excel_file_path, function_name, code_path)  #FI5 não coberto
+    if(error):  
+        adicionar_ao_log(error)
+        return
+    
     adicionar_ao_log("Test Driver criado com sucesso.")
-
+    
     Run_Test_Driver(compiler)
 
 def relatorio():
@@ -23,10 +25,10 @@ def relatorio():
 if __name__ == '__main__':
 
     # Defina o caminho para o arquivo Excel
-    excel_file_path = r"examples\C_proj_mockup\TestInputs\new_testvec3.xlsx"
+    excel_file_path = "examples/C_proj_mockup/TestInputs/new_testvec1.xlsx"
 
     # Defina o nome do arquivo .c do SUT
-    code_path = "examples\C_proj_mockup\SUT\SUT.c" 
+    code_path = "examples/C_proj_mockup/SUT/SUT2.c" 
 
     # Defina o nome da função testada
     function_name = "SUT"
--- src/run_test_drive.py
+++ src/run_test_drive.py
@@ -5,20 +5,27 @@ def Run_Test_Driver(compiler):
     adicionar_ao_log("Executando Test Driver...")
     match compiler:
         case "gcc":
-            #Compila o programa C
-            subprocess.run(["gcc", "output\InstrumentedSUT\instrumented_SUT.c", "output\InstrumentedSUT\Test_Driver.c", "-o", "output\TestDriver\Test_Driver"], check=True)  #-o 
+            try:
+                #Compila o programa C
+                subprocess.run(["gcc", "output/InstrumentedSUT/instrumented_SUT.c", "output/InstrumentedSUT/Test_Driver.c", "-o", "output/TestDriver/Test_Driver"], check=True, text=True, capture_output=True) 
 
-            # Executa o programa C
-            subprocess.run([".\output\TestDriver\Test_Driver.exe"], check=True) 
+                # Executa o programa C
+                subprocess.run(["./output/TestDriver/Test_Driver.exe"], check=True) 
+            except subprocess.CalledProcessError as e:
+                error = f"ERROR: TestDrive não executado corretamente." # {e.stderr}
+                adicionar_ao_log(error)
+                return error
             
         
         case "clang":   #OPÇÃO NÃO TESTADA
             #Compila o programa C
-            subprocess.run(["clang", "output\InstrumentedSUT\instrumented_SUT.c", "output\TestDriver\Test_Driver.c", "-o", "Test_Driver"], check=True)  
+            subprocess.run(["clang", "output/InstrumentedSUT/instrumented_SUT.c", "output/TestDriver/Test_Driver.c", "-o", "Test_Driver"], check=True)  
 
             # Executa o programa C
-            subprocess.run([".\Test_Driver.exe"], check=True)
+            subprocess.run(["./Test_Driver.exe"], check=True)
+
     adicionar_ao_log("Test Driver executado com sucesso.")
+    return 0
 
 
 if __name__ == '__main__':

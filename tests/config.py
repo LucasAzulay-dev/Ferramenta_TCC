b9f223b73ef27ddeb32228378bb7598039ccdc0e
gabrielAraoUfpe <gca3@cin.ufpe.br>
2024-11-11 22:18:09 -0300

    Testes acessam log de execução e faz parse do JSON

---
+++ tests/config.py
@@ -0,0 +1,16 @@
+# Config output messages and file paths.
+
+MSG_INSTRUMENTATION_START = "Iniciando instrumentação do código..."
+MSG_INSTRUMENTATION_END = "Instrumentação concluída."
+MSG_HEADER_CREATING = "Gerando arquivo .h com pycparser..."
+MSG_HEADER_CREATED = "Arquivo .h gerado com sucesso."
+MSG_DRIVER_CREATING = "Criando Test Driver..."
+MSG_DRIVER_CREATED = "Test Driver criado com sucesso."
+MSG_DRIVER_EXECUTING = "Executando Test Driver..."
+
+PATH_INSTRUMENTED_SUT_C = "output\\InstrumentedSUT\\instrumented_SUT.c"
+PATH_INSTRUMENTED_SUT_H = "output\\InstrumentedSUT\\instrumented_SUT.h"
+PATH_TEST_DRIVER_C = "output\\InstrumentedSUT\\Test_Driver.c"
+PATH_TEST_DRIVER_EXE = "output\\TestDriver\\Test_Driver.exe"
+PATH_LOG_BUFFER = "output\\OutputBuffer\\log_buffer.txt"
+PATH_REPORT_PDF = "output\\Report\\dc_cc_analysis_report.pdf"
--- tests/test_Instrumentation.py
+++ tests/test_Instrumentation.py
@@ -1,4 +1,5 @@
 import pytest
+from .config import *
 from src.Ferramenta_TCC import executar_ferramenta
 from .conftest import ToolParameters
 import os.path
@@ -6,36 +7,35 @@ import os.path
 # FI#1: The Tool must accept a SUT, the name of the function to be tested, a test suite and a compiler name as input.
 class TestFI_1:
     # The required inputs are accepted. Using GCC compiler
-    def test_1(self, param_success):
+    def test_1(self, param_success, assert_output):
         param : ToolParameters = param_success
         executar_ferramenta(excel_file_path=param.testvec, 
                             code_path=param.sut_path, 
                             function_name=param.sut_name, 
                             compiler=param.compiler)
+        assert_output(MSG_INSTRUMENTATION_START)
         
 # FI#2: The Tool must generate a Test Driver based on its inputs
 class TestFI_2:
     def test_1(self, param_success):
         param : ToolParameters = param_success
+        executar_ferramenta(excel_file_path = param.testvec, 
+                            code_path = param.sut_path, 
+                            function_name = param.sut_name, 
+                            compiler = param.compiler)
         
-        executar_ferramenta(excel_file_path=param.testvec, 
-                            code_path=param.sut_path, 
-                            function_name=param.sut_name, 
-                            compiler=param.compiler)
-        
-        assert os.path.exists('output\\InstrumentedSUT\\Test_Driver.c') and os.path.exists('output\\TestDriver\\Test_Driver.exe')
+        assert os.path.exists(PATH_TEST_DRIVER_C) and os.path.exists(PATH_TEST_DRIVER_EXE)
 
 # FI#3: The Tool must generate instrumented code from the SUT.
 class TestFI_3:
     def test_1(self, param_success):
         param : ToolParameters = param_success
+        executar_ferramenta(excel_file_path = param.testvec, 
+                            code_path = param.sut_path, 
+                            function_name = param.sut_name, 
+                            compiler = param.compiler)
         
-        executar_ferramenta(excel_file_path=param.testvec, 
-                            code_path=param.sut_path, 
-                            function_name=param.sut_name, 
-                            compiler=param.compiler)
-        
-        assert os.path.exists('output\\InstrumentedSUT\\instrumented_SUT.c')
+        assert os.path.exists(PATH_INSTRUMENTED_SUT_C)
 
 # FI#4: The Test Driver must execute tests using the instrumented code.
 class TestFI_4:
@@ -44,7 +44,25 @@ class TestFI_4:
 # FI#5: The Test Driver must be able to identify and flag invalid or unexpected 
 # test inputs without compromising the execution of subsequent tests.
 class TestFI_5:
-    pass
+    # On success, no skipped tests
+    def test_1(self, param_success, get_json):
+        param : ToolParameters = param_success
+        executar_ferramenta(excel_file_path = param.testvec, 
+                            code_path = param.sut_path, 
+                            function_name = param.sut_name, 
+                            compiler = param.compiler)
+        json_out = get_json()
+        assert json_out['skipedlines'] == []
+
+    # With some faulty tests, some are skipped but execution has no errors
+    def test_2(self, param_invalid_line, get_json):
+        param : ToolParameters = param_invalid_line
+        executar_ferramenta(excel_file_path = param.testvec, 
+                            code_path = param.sut_path, 
+                            function_name = param.sut_name, 
+                            compiler = param.compiler)
+        json_out = get_json()
+        assert json_out['skipedlines'] != []
 
 # FI#6: The Test Driver must generate an execution log of the tests performed on the instrumented code.
 class TestFI_6:

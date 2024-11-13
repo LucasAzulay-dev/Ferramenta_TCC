c8e6bed9dbfbf386eea728ba8a4c2dcae1f6383e
Lucas Azulay <lsa6@cin.ufpe.br>
2024-11-13 09:37:01 -0300

    Correção pasta output

--- src/run_test_drive.py
+++ src/run_test_drive.py
@@ -20,13 +20,22 @@ def Run_Test_Driver(folder_path, SUT_path, compiler):
                 adicionar_ao_log(error)
                 return error
             
-        
         case "clang":   #OPÇÃO NÃO TESTADA
-            #Compila o programa C
-            subprocess.run(["clang", "output/InstrumentedSUT/instrumented_SUT.c", "output/TestDriver/Test_Driver.c", "-o", "Test_Driver"], check=True)  
+            try:
+                #Compila o programa C
+                compile_path = list_c_directories(folder_path, SUT_path) + list_c_files(folder_path, SUT_path)
+                args = ['clang']
+                args = args + (list(filter(None, compile_path))) + ["output/InstrumentedSUT/instrumented_SUT.c", "output/InstrumentedSUT/Test_Driver.c", "-o", "output/TestDriver/Test_Driver"]
+                print(args)
+                subprocess.run(args, check=True, text=True, capture_output=True) 
 
-            # Executa o programa C
-            subprocess.run(["./Test_Driver.exe"], check=True)
+                # Executa o programa C
+                subprocess.run(["./output/TestDriver/Test_Driver.exe"], check=True)
+
+            except subprocess.CalledProcessError as e:
+                error = f"ERROR: TestDrive não executado corretamente.{e.stderr}" # {e.stderr}
+                adicionar_ao_log(error)
+                return error
 
     adicionar_ao_log("Test Driver executed successfully.")
     return 0
@@ -38,6 +47,6 @@ if __name__ == '__main__':
     SUT_path = "tests/test_cases/case1/SUT/SUT.c"
 
     #Tipo de compilador
-    compiler = "gcc"    #gcc ou clang
+    compiler = "clang"    #gcc ou clang
 
     Run_Test_Driver(folder_path, SUT_path, compiler)

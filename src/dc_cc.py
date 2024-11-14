832b5e995df534512dfbd59882a853470f283f10
theudelci <theudelci@gmail.com>
2024-11-14 15:55:39 -0300

    add to gui log dc cc report datas

--- src/dc_cc.py
+++ src/dc_cc.py
@@ -5,6 +5,7 @@ import re
 from dc_cc_diagram import diagram_generator
 from dc_cc_analysis import CouplingAnalyzer
 from dc_cc_report import create_report
+from utils import adicionar_ao_log
 
 def corrigir_virgulas(conteudo):
     return re.sub(r',(\s*[\}\]])', r'\1', conteudo)
@@ -17,11 +18,13 @@ def DC_CC_Report_Generator(log_buffer_path):
         except:
             error = "ERROR: Failed to load log_buffer. Consider increasing the buffer length."
             raise Exception(error)
- 
+
+        adicionar_ao_log("Generating SUT Diagram")
         diagram_directory = 'output/Report/'
         diagram_filename = 'diagram'
         diagram_generator(log_data, diagram_directory, diagram_filename)
         path_diagram = diagram_directory + diagram_filename + '.pdf'
+        adicionar_ao_log("SUT Diagram sucessfully generated")
         
         analyzer = CouplingAnalyzer(log_data)
         data = analyzer.identify_couplings_exercised()

2b0d94c82570561b1c669810fe0ad2f25e0f9b01
gabrielAraoUfpe <gca3@cin.ufpe.br>
2024-11-19 11:50:48 -0300

    Improving robustness tests

--- tests/test_robustness.py
+++ tests/test_robustness.py
@@ -22,55 +22,55 @@ class TestFI_5:
     # all lines are invalid in testvec
     def test_3(self, execute_robust_case):
         robustness_case = 'testvec_all_invalid_lines'
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as exception:
             execute_robust_case(robustness_case)
-        # assert_output(ERROR_NO_VALID_LINES)
+        assert str(exception.value) == ERROR_NO_VALID_LINES
     
     # SUT function not found
     def test_4(self, execute_robust_case):
         robustness_case = 'sut_function_not_found'
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as exception:
             execute_robust_case(robustness_case)
-        # assert_output(ERROR_SUT_FCN_NOT_FOUND)
+        assert str(exception.value) == ERROR_SUT_FCN_NOT_FOUND
     
     # SUT not testable
     def test_5(self, execute_robust_case):
         robustness_case = 'sut_not_testable'
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as exception:
             execute_robust_case(robustness_case)
-        # assert_output(ERROR_SUT_NOT_TESTABLE)
+        assert str(exception.value) == ERROR_SUT_NOT_TESTABLE
 
     # SUT with runtime error
     def test_6(self, execute_robust_case):
         robustness_case = 'sut_runtime_error'
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as exception:
             execute_robust_case(robustness_case)
-        # assert_output(ERROR_EXEC_ERROR)
+        assert str(exception.value) == ERROR_EXEC_ERROR
     
     # SUT is not a C file
     def test_7(self, execute_robust_case):
         robustness_case = 'sut_wrong_type'
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as exception:
             execute_robust_case(robustness_case)
-        # assert_output(ERROR_SUT_WRONG_TYPE)
+        assert str(exception.value) == ERROR_SUT_WRONG_TYPE
     
     # Tesvec is not an excel file
     def test_8(self, execute_robust_case):
         robustness_case = 'testvec_wrong_type'
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as exception:
             execute_robust_case(robustness_case)
-        # assert_output(ERROR_TESTVEC_WRONG_TYPE)
+        assert str(exception.value) == ERROR_TESTVEC_WRONG_TYPE
     
     # Tesvec is missing columns
     def test_9(self, execute_robust_case):
         robustness_case = 'testvec_missing_column'
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as exception:
             execute_robust_case(robustness_case)
-        # assert_output(ERROR_TESTVEC_MISSING_COL)
+        assert str(exception.value) == ERROR_TESTVEC_MISSING_COL
             
     # log buffer is just 1 byte long
     def test_10(self, execute_robust_case):
         robustness_case = 'log_buffer_load_error'
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as exception:
             execute_robust_case(robustness_case)
-        # assert_output(ERROR_LOG_BUFFER_LOAD_ERROR)
+        assert str(exception.value) == ERROR_LOG_BUFFER_LOAD_ERROR
